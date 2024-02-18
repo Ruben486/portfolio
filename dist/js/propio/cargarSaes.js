54fe45e9ca1ae38c7d708fa9d0d3e121496b71b9
Ruben <arubensanchezg@gmail.com>
2024-02-18 00:54:35 -0300

    18/02/2027-1

--- dist/js/propio/cargarSaes.js
+++ dist/js/propio/cargarSaes.js
@@ -1,37 +1,60 @@
-
-const cargarDatos = async () => {
-  console.log(datosSaes)
-  try {
-    const URI = "../../../src/jsonsaes.json";
-    const respuesta = await fetch(URI);
-    if (respuesta.status === 200) {
-      const data = await respuesta.json();
-      return data;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const sectionSaes = document.getElementById("grilla-saes");
-const armarSAES = async () => {
-  const datos = await cargarDatos();
-  const datosHtml = datos
+const datosSaes = [
+  {
+    img: "../public/images/aes/aes-2.jpg",
+    desc: "json-1.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-3.jpg",
+    desc: "json-2.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-4.jpg",
+    desc: "json-3.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-5.jpg",
+    desc: "json-4.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-6.jpg",
+    desc: "jsn-5.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-7.jpg",
+    desc: "json-6.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-8.jpg",
+    desc: "json-7.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-9.jpg",
+    desc: "json-8.jpg",
+  },
+  {
+    img: "../public/images/aes/aes-10.jpg",
+    desc: "json-9.jpg",
+  },
+];
+const armarSAES = () => {
+  console.log(datosSaes);
+  const sectionSaes = document.getElementById("grilla-saes");
+  const datosHtml = datosSaes
     .map(
       (dato, index) => `<div class= "g-celda"> 
-    <img id='img-${index + 2}' class="img-saes" src='${dato.img}'
-    />
-   <p class="p-saes"> ${dato.desc}</p>
-    </div>`
+      <img id='img-${index + 2}' class="img-saes" src='${dato.img}'
+      />
+     <p class="p-saes"> ${dato.desc}</p>
+      </div>`
     )
     .join(" ");
-    
+
   sectionSaes.innerHTML = datosHtml;
   handleModal();
 };
 
 var modal = document.getElementById("ventanaModal");
-var span  = document.getElementsByClassName("cerrar")[0];
+var span = document.getElementsByClassName("cerrar")[0];
 var imgModal = document.getElementById("imgModal");
 // el uusario cierre la venta en la cruz //
 span.addEventListener("click", function () {
@@ -58,9 +81,10 @@ const handleModal = () => {
     divs[i].addEventListener("click", function () {
       const imagen = divs[i].children[0].attributes[2].nodeValue;
       const parrafo = divs[i].children[1].childNodes[0].nodeValue;
-      abrirModal(imagen,parrafo);
+      abrirModal(imagen, parrafo);
     });
   }
 };
-
-armarSAES();
+if (datosSaes.length > 0) {
+  armarSAES();
+}
